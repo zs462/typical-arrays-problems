64fdcc1c37cb9ad640baccddd84162d6b747e242
Siarhei Zaretsky <z__s@mail.ru>
2020-10-03 20:17:11 +0300

    feat: implement min, max, avg functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,53 @@
 
 exports.min = function min (array) {
-  return 0;
+
+    let minItem = undefined
+    if (Array.isArray(array) && array) {
+        for (let item of array) {
+            if (minItem === undefined) {
+                minItem = item
+            } else if (item < minItem) {
+                minItem = item
+            }
+        }
+    }
+    if (minItem === undefined) {
+        return 0;
+    }
+
+    return minItem;
 }
 
 exports.max = function max (array) {
-  return 0;
+
+    let maxItem = undefined
+    if (Array.isArray(array) && array) {
+        for (let item of array) {
+            if (maxItem === undefined) {
+                maxItem = item
+            } else if (item > maxItem) {
+                maxItem = item
+            }
+        }
+    }
+    if (maxItem === undefined) {
+        return 0;
+    }
+
+    return maxItem;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+
+    let sum = 0;
+    if (Array.isArray(array) && array) {
+        for (let item of array) {
+            sum += item
+        }
+        if (array.length) {
+            return sum / array.length
+        }
+    }
+    return 0;
+
 }
